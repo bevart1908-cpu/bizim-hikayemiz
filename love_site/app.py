9ac184d20ccc7ba85ccbc0b5c91fac8ba1a39264
bevart1908-cpu <bevart1908@gmail.com>
2025-12-31 16:11:27 +0300

    app.py

--- love_site/app.py
+++ love_site/app.py
@@ -20,5 +20,10 @@ def home():
         kilitli=kilitli
     )
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
